2502da3acacef4ece9d9a896617f9652db31eb2c
Bart te Lindert <btlindert@users.noreply.github.com>
2015-01-30 15:38:27 +0100

    Minor changes

--- +btmn/+split_files/block_split_main.m
+++ +btmn/+split_files/block_split_main.m
@@ -11,14 +11,17 @@ import btmn.*;
 
 %% Splitting parameters
 
-SUBJECTS = 9;
-
+SUBJECTS   = 9:10;
+SESSION    = 'morning';
 OUTPUT_DIR = '/data2/projects/btmn/analysis/splitting/blocks/';
         
 %% Select the relevant data files
 
-files = link2rec('btmn', 'file_ext', '.mff', 'subject', SUBJECTS, ...
-    'session', 'morning', 'folder', OUTPUT_DIR);
+files = link2rec('btmn',... 
+    'file_ext', '.mff',... 
+    'subject',  SUBJECTS, ...
+    'session',  SESSION, ... 
+    'folder',   OUTPUT_DIR);
         
 %% Process all files with the splitting pipeline
 
@@ -28,8 +31,8 @@ USE_OGE   = true;
 QUEUE     = 'meegpipe.q';
 
 myPipe = btmn.pipes.block_split_pipeline(...
-    'GenerateReport',   DO_REPORT, ...
-    'OGE',              USE_OGE, ...
-    'Queue',            QUEUE);
+    'GenerateReport', DO_REPORT, ...
+    'OGE',            USE_OGE, ...
+    'Queue',          QUEUE);
 
 run(myPipe, files{:});
